8615f088171b8ace5b17e26fb78e726a1b8f4826
junghunchoi <azzudi@gmail.com>
2022-11-10 17:45:25 +0900

    1110 commit

--- src/main/java/junghun/workbook/controller/UpDownController.java
+++ src/main/java/junghun/workbook/controller/UpDownController.java
@@ -3,18 +3,24 @@ package junghun.workbook.controller;
 
 import io.swagger.annotations.ApiOperation;
 import junghun.workbook.dto.upload.UpLoadFileDTO;
+import junghun.workbook.dto.upload.UploadResultDTO;
+import junghun.workbook.entity.Board;
 import lombok.extern.log4j.Log4j2;
+import net.coobird.thumbnailator.Thumbnailator;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.UUID;
+import java.util.*;
 
 @RestController
 @Log4j2
@@ -25,10 +31,14 @@ public class UpDownController {
 
 	@ApiOperation(value = "Upload Post", notes = "Post 방식으로 파일 등록")
 	@PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-	public String upload(UpLoadFileDTO upLoadFileDTO) {
+	public List<UploadResultDTO> upload(UpLoadFileDTO upLoadFileDTO) {
+
 		log.info(upLoadFileDTO);
 
 		if (upLoadFileDTO.getFiles() != null) {
+
+			final List<UploadResultDTO> list = new ArrayList<>();
+
 			upLoadFileDTO.getFiles().forEach(multipartFile -> {
 
 				String orginalName = multipartFile.getOriginalFilename();
@@ -39,21 +49,79 @@ public class UpDownController {
 
 				Path savePath = Paths.get(uploadPath, uuid + "_" + orginalName);
 
+				boolean image = false;
+
 				try {
 					multipartFile.transferTo(savePath);
 
+					// 이미지 파일의 종류라면
 					if (Files.probeContentType(savePath).startsWith("image")) {
 
-						File thumbFile = new File(uploadPath, "s_");
+						image = true;
+
+						File thumbFile = new File(uploadPath, "s_" + uuid + "_" + orginalName);
+
+						Thumbnailator.createThumbnail(savePath.toFile(), thumbFile, 200, 200);
 					}
 				} catch (IOException e) {
 					e.printStackTrace();
 				}
 
-			});
-		}
+				list.add(UploadResultDTO.builder()
+						.uuid(uuid)
+						.fileName(orginalName)
+						.img(image)
+						.build()
+				);
+
+			}); // end each
+			return list;
+		}//end if
 
 		return null;
+	}
+
+	@ApiOperation(value = "view 파일", notes = "Get 방식으로 첨부파일 조회")
+	@GetMapping("/view/{fileName}")
+	public ResponseEntity<Resource> viewFileGet(@PathVariable String fileName) {
+		Resource resource = new FileSystemResource(uploadPath + File.separator + fileName); // 파일경로라고 생각하면 될듯.
+		String resourceName = resource.getFilename();
+		HttpHeaders headers = new HttpHeaders();
+
+		try {
+			headers.add("Content-type", Files.probeContentType(resource.getFile().toPath())); // 파일의 형식을 조사하는 메소드
+		} catch (Exception e) {
+			return ResponseEntity.internalServerError().build();
+		}
+		return ResponseEntity.ok().headers(headers).body(resource);
+	}
+
+	@ApiOperation(value = "remove 파일", notes = "delete 방식으로 파일삭제")
+	@DeleteMapping("/remove/{fileName}")
+	public Map<String, Boolean> removeFile(@PathVariable String fileName) {
+
+		Resource resource = new FileSystemResource(uploadPath + File.separator + fileName);
+
+		String resourceName = resource.getFilename();
+
+		Map<String, Boolean> resultMap = new HashMap<>();
+		boolean removed = false;
+
+		try {
+
+			String contentType = Files.probeContentType(resource.getFile().toPath());
+			removed = resource.getFile().delete();
+
+			// 만약 섬네일이 존재하면
+			if (contentType.startsWith("image")) {
+				File thumbnailFile = new File(uploadPath + File.separator + fileName);
+				thumbnailFile.delete();
+			}
+		} catch (Exception e) {
+			log.error(e.getMessage());
+		}
+		resultMap.put("result", removed);
+		return resultMap;
 	}
 
 }
