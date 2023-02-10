b2a8feedfa150a2a723f4ff601aded6dc8315485
junghunchoi <azzudi@gmail.com>
2023-02-10 19:37:48 +0900

    02.10 저녁 7시 카페에서

--- src/main/java/junghun/workbook/controller/ReplyController.java
+++ src/main/java/junghun/workbook/controller/ReplyController.java
@@ -1,13 +1,25 @@
 package junghun.workbook.controller;
 
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import java.util.HashMap;
 import java.util.Map;
+import javax.validation.Valid;
+import junghun.workbook.dto.PageRequestDTO;
+import junghun.workbook.dto.PageResponseDTO;
 import junghun.workbook.dto.ReplyDTO;
+import junghun.workbook.entity.Reply;
 import junghun.workbook.service.ReplyService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.data.domain.Page;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,10 +37,45 @@ public class ReplyController {
     @ApiOperation(value = "Replies POST", notes = "POST 방식으로 댓글등록")
     @PostMapping(value = "/", consumes = MediaType.APPLICATION_JSON_VALUE)
 
-    public ResponseEntity<Map<String, Long>> register(@RequestBody ReplyDTO replyDTO) {
+    public Map<String, Long> register(@Valid @RequestBody ReplyDTO replyDTO, BindingResult bindingResult) throws BindException {
 
-        Map<String, Long> resultMap = Map.of("rno", 111L);
-        return ResponseEntity.ok(resultMap);
+        if (bindingResult.hasErrors()) {
+            throw new BindException(bindingResult);
+        }
+        Map<String, Long> resultMap = new HashMap<>();
+
+        Long rno = replyService.Register(replyDTO);
+
+        resultMap.put("rno", rno);
+
+        return resultMap;
     }
 
+    @ApiOperation(value="Replies of Board", notes = "GET 방식으로 특정 게시물 댓글목록")
+    @GetMapping(value="/list/{bno}")
+    public PageResponseDTO<ReplyDTO> getList(@PathVariable("bno") Long bno, PageRequestDTO pageRequestDTO    ) {
+        PageResponseDTO<ReplyDTO> requestDTO = replyService.getListOfBoard(bno, pageRequestDTO);
+
+        return requestDTO;
+
+    }
+
+    @ApiOperation(value = "Read Only", notes = "GET 방식으로 특정 댓글조회")
+    @GetMapping("/{rno}")
+    public ReplyDTO getReplyDTO(@PathVariable("rno") Long rno) {
+
+        ReplyDTO replyDTO = replyService.read(rno);
+
+        return replyDTO;
+    }
+
+    @DeleteMapping("/{rno}")
+    public Map<String,Long> remove (@PathVariable("rno") Long rno) {
+        replyService.remove(rno);
+
+        Map<String, Long> resultMap = new HashMap<>();
+        resultMap.put("rno", rno);
+
+        return resultMap;
+    }
 }
--- src/main/java/junghun/workbook/controller/advice/CustomRestAdvice.java
+++ src/main/java/junghun/workbook/controller/advice/CustomRestAdvice.java
@@ -4,7 +4,9 @@ package junghun.workbook.controller.advice;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.NoSuchElementException;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -29,6 +31,31 @@ public class CustomRestAdvice {
         }
 
         return ResponseEntity.badRequest().body(errorMap);
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    @ResponseStatus(HttpStatus.EXPECTATION_FAILED)
+    public ResponseEntity<Map<String, String>> handleFKException(Exception e) {
+
+        Map<String, String> errorMap = new HashMap<>();
+
+        errorMap.put("time", "" + System.currentTimeMillis());
+        errorMap.put("msg", "constraint fail");
+        return ResponseEntity.badRequest().body(errorMap);
+
+    }
+
+    // 서비스계층에서 에러가 났을 경우 컨트롤러에 전해지기전에 return되도록
+    @ExceptionHandler({NoSuchElementException.class})
+    @ResponseStatus(HttpStatus.EXPECTATION_FAILED)
+    public ResponseEntity<Map<String,String>> handleNosuchElement(Exception e) {
+        Map<String, String> errorMap = new HashMap<>();
+
+        errorMap.put("time", "" + System.currentTimeMillis());
+        errorMap.put("msg", "No Such Element");
+
+        return ResponseEntity.badRequest().body(errorMap);
+
     }
 
 }
